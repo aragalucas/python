a3bf92b31fd204a724bc851c51569c915e1b2cc4
aragalucas <126295197+aragalucas@users.noreply.github.com>
2024-04-25 13:17:03 +0000

    terminei o progama de login

---
+++ login.py
@@ -0,0 +1,8 @@
+login = input("login: ")
+senha = input("senha: ")
+loginCorreto = "correto"
+senhaCorreta = "certo"
+if login == loginCorreto and senha == senhaCorreta:
+    print("acesso permitido")
+else:
+    print("acesso negado")
