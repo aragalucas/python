047d1ebdb73d486d09a9e94d9046b38e661e785a
aragalucas <126295197+aragalucas@users.noreply.github.com>
2024-05-23 11:18:36 +0000

    more pratical exercices

---
+++ function.py
@@ -0,0 +1,11 @@
+import os
+def logosenai():
+    os.system("cls || clear")
+    print("=== ===== ===")
+    print("=== SENAI ===")
+    print("=== ===== ===")
+
+logosenai()
+nome = input("nome: ")
+logosenai()
+idade = int(input("idade: "))
--- maiormenorcomlista.py
+++ maiormenorcomlista.py
@@ -1,7 +1,8 @@
 numeros = []
 maiorNumero = 0
 menorNumero = 2000000
-for i in range(5):
+QUANTIDADE_NOTAS = 5
+for i in range(QUANTIDADE_NOTAS):
     numero = float(input(f"digite o numero {i + 1}: "))
     numeros.append(numero)
     if numero > maiorNumero:
@@ -10,5 +11,5 @@ for i in range(5):
         menorNumero = numero
 print(f"maior numero {maiorNumero}")
 print(f"menor numrero {menorNumero}")
-for i in range(5):
+for i in range(QUANTIDADE_NOTAS):
     print(f"numero {i + 1}: {numeros[i]}")
