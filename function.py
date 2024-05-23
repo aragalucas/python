dbeb7310d147bfb32cefe638a40b5ce43306d449
aragalucas <126295197+aragalucas@users.noreply.github.com>
2024-05-23 11:46:23 +0000

    adicionei a função com retorno a o arquivo function

--- function.py
+++ function.py
@@ -1,11 +1,24 @@
 import os
+# função sem retorno
 def logosenai():
     os.system("cls || clear")
     print("=== ===== ===")
     print("=== SENAI ===")
     print("=== ===== ===")
+    
+#função com retorno
+def somar(n1, n2):
+    resultado = n1 + n2
+    return resultado
 
 logosenai()
 nome = input("nome: ")
 logosenai()
 idade = int(input("idade: "))
+logosenai()
+primeiroNumero = int(input("numero 1: "))
+logosenai()
+segundoNumero = int(input("numero 2: "))
+soma = somar(primeiroNumero, segundoNumero)
+logosenai()
+print(f"soma: {soma}")
